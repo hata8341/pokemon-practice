3dbef538c9a51a63dc7ffe94d109a84803de3215
hata <wmf8341@gmail.com>
2022-04-24 13:59:47 +0900

    step4

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-import './poke_detail.dart';
 import './poke_list_item.dart';
 
 void main() {
@@ -13,10 +12,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+      title: 'Pokemon Flutter',
+      theme: ThemeData.light(),
+      darkTheme: ThemeData.dark(),
+      themeMode: ThemeMode.system,
       home: const TopPage(),
     );
   }
@@ -35,34 +34,3 @@ class TopPage extends StatelessWidget {
     );
   }
 }
-
-// class PokeListItem extends StatelessWidget {
-//   const PokeListItem({Key? key, required this.index}) : super(key: key);
-//   final int index;
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return ListTile(
-//         leading: Container(
-//           width: 80,
-//           decoration: BoxDecoration(
-//               color: Colors.yellow.withOpacity(.5),
-//               borderRadius: BorderRadius.circular(10),
-//               image: const DecorationImage(
-//                   fit: BoxFit.fitWidth,
-//                   image: NetworkImage(
-//                     "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
-//                   ))),
-//         ),
-//         title: const Text(
-//           'pikachu',
-//           style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-//         ),
-//         subtitle: const Text('⚡️electric'),
-//         trailing: const Icon(Icons.navigate_next),
-//         onTap: () => {
-//               Navigator.of(context).push(MaterialPageRoute(
-//                   builder: (BuildContext context) => const PokeDetail()))
-//             });
-//   }
-// }
