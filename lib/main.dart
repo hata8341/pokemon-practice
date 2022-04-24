f7c4464b87946f145f4aa924bee5d859fa1e9fdf
hata <wmf8341@gmail.com>
2022-04-24 16:05:27 +0900

    step5

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
-import './poke_list_item.dart';
+import './poke_list.dart';
+import './settings.dart';
 
 void main() {
   runApp(const MyApp());
@@ -21,15 +22,38 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class TopPage extends StatelessWidget {
+class TopPage extends StatefulWidget {
   const TopPage({Key? key}) : super(key: key);
 
   @override
+  _TopPageState createState() => _TopPageState();
+}
+
+class _TopPageState extends State<TopPage> {
+  int currentbnb = 0;
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView.builder(
-        itemCount: 10000,
-        itemBuilder: (context, index) => PokeListItem(index: index),
+      body: SafeArea(
+        child: currentbnb == 0 ? const PokeList() : const Settings(),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: (index) => {
+          setState(
+            () {
+              print(index);
+              currentbnb = index;
+            },
+          )
+        },
+        currentIndex: currentbnb,
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.list), label: 'home'),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: 'settings',
+          ),
+        ],
       ),
     );
   }
